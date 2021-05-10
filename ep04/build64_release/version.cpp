fc2d6425e8fc7b90a900f291a126615fc6f5ea46
MaxEntroy <likang@tju.edu.cn>
2021-05-10 11:51:24 +0800

    Update gitignore.

--- ep04/build64_release/version.cpp
+++ ep04/build64_release/version.cpp
@@ -4,14 +4,14 @@ namespace binary_version {
 extern const int kSvnInfoCount = 1;
 extern const char* const kSvnInfo[1] = {"git@github.com:MaxEntroy/pnp.git\n"
 "* main\n"
-"commit a4cf0ae8d697726f066972269111d1b300ab630d\n"
+"commit 8d0b56a5d731dbba04ce030fcedd78bf2ca5d9b9\n"
 "Author: MaxEntroy <likang@tju.edu.cn>\n"
-"Date:   Tue Apr 27 14:45:51 2021 +0800\n"
+"Date:   Wed Apr 28 11:58:57 2021 +0800\n"
 "\n"
-"    Update README in ep04.\n"
+"    Add demo-00 in ep04.\n"
 ""};
 extern const char kBuildType[] = "release";
-extern const char kBuildTime[] = "Wed Apr 28 11:58:22 2021";
+extern const char kBuildTime[] = "Mon May 10 11:48:08 2021";
 extern const char kBuilderName[] = "kang";
 extern const char kHostName[] = "ubuntu";
 extern const char kCompiler[] = "GCC 4.8";
